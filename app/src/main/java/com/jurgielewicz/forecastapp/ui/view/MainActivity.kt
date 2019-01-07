f95ecc4299f0c284399fc0ab7f23aaa4f8d97350
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2019-01-07 02:01:04 +0100

    Added viewpager and fragments layout

--- app/src/main/java/com/jurgielewicz/forecastapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/jurgielewicz/forecastapp/ui/view/MainActivity.kt
@@ -1,15 +1,10 @@
-package com.jurgielewicz.forecastapp
+package com.jurgielewicz.forecastapp.ui.view
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.jurgielewicz.forecastapp.R
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
