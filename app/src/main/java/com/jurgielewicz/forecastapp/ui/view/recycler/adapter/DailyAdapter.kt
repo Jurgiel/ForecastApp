e7896dcf6d83867f76485597b0436ad25ce86160
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2019-01-09 20:22:33 +0100

    Daily recyclerview

---
+++ app/src/main/java/com/jurgielewicz/forecastapp/ui/view/recycler/adapter/DailyAdapter.kt
@@ -0,0 +1,26 @@
+package com.jurgielewicz.forecastapp.ui.view.recycler.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.jurgielewicz.forecastapp.R
+import com.jurgielewicz.forecastapp.dataModel.Response
+import com.jurgielewicz.forecastapp.ui.view.recycler.holder.DailyHolder
+
+class DailyAdapter(private val list: List<Response>?): RecyclerView.Adapter<DailyHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val weatherRow = layoutInflater.inflate(R.layout.row_daily_weather, parent, false)
+
+        return DailyHolder(weatherRow)
+    }
+
+    override fun getItemCount(): Int {
+        return list?.get(0)?.periods?.lastIndex!! //cant return Int?
+    }
+
+    override fun onBindViewHolder(holder: DailyHolder, position: Int) {
+        val data = list?.get(0)?.periods?.get(position)
+        holder.bindItem(data)
+    }
+}
---
+++ app/src/main/java/com/jurgielewicz/forecastapp/ui/view/recycler/holder/DailyHolder.kt
@@ -0,0 +1,18 @@
+package com.jurgielewicz.forecastapp.ui.view.recycler.holder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.jurgielewicz.forecastapp.dataModel.Periods
+import kotlinx.android.synthetic.main.row_daily_weather.view.*
+
+class DailyHolder( view: View): RecyclerView.ViewHolder(view) {
+    private var view: View = view
+
+    fun bindItem(item: Periods?){
+        view.weatherTextView_daily_weather_row.text = item?.weather
+        view.maxTempTextView_daily_weather_row.text = item?.maxTempC.toString().plus("℃\t")
+        view.minTempTextView_daily_weather_row.text = item?.minTempC.toString().plus("℃\t")
+//        view.day_daily_weather_row.text = timeConverter(item?.dateTimeISO, 1)
+//        view.dateTextView_Daily_Weather_Row.text = timeConverter(item?.dateTimeISO, 0)
+    }
+}
