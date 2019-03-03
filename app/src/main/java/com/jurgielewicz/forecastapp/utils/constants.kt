e91b462e5ca7c83836ce1328887804a522d4e983
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2019-03-03 17:25:26 +0100

    const, api key

--- app/src/main/java/com/jurgielewicz/forecastapp/utils/constants.kt
+++ app/src/main/java/com/jurgielewicz/forecastapp/utils/constants.kt
@@ -1,3 +1,5 @@
 package com.jurgielewicz.forecastapp.utils
 
-val BASE_URL = ""
+const val BASE_URL = "https://api.aerisapi.com/"
+const val CLIENT_ID = "eTUf2m5i8aup1Z9lyGBYa"
+const val CLIENT_SECRET = "xHmZ8Zr3GxadZ4zwYzqBjRJNYuL1SqJEYxYetI8S"
